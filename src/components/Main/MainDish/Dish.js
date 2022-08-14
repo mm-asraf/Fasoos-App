49dc63b481c23c5d044bba38e90645254c64b60c
Mahboob Asraf <mahboobasraf@gmail.com>
2022-08-14 18:55:10 +0530

    UI

--- src/components/Main/MainDish/Dish.js
+++ src/components/Main/MainDish/Dish.js
@@ -1,9 +1,13 @@
 import React from "react";
 import MainCard from "../../UI/MainCard";
 import classes from "./Dish.module.css";
-
+import Card from "../../UI/Card";
 const Dish = () => {
-  return <MainCard className={classes.card}></MainCard>;
+  return (
+    <MainCard className={classes.card}>
+      <Card className={classes.card_set}>h</Card>
+    </MainCard>
+  );
 };
 
 export default Dish;
